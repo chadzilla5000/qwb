cf0308f3eabf17202e0edef883a572697b14c513
chadzilla5000 <131494809+chadzilla5000@users.noreply.github.com>
2023-04-26 17:21:57 -0400

    Adding Files via upload
    Added more files part 2 of the upload and start of quickbooks files.

---
+++ recalcost.php
@@ -0,0 +1,21 @@
+<?php
+include_once('inc/functions/general.php');
+include_once 'inc/functions.php';
+error_reporting(E_ALL | E_STRICT);
+
+include_once 'inc/functions.php';
+require_once 'wconfig.php';
+
+//echo '$' . 34;
+	if(isset($_GET['lp'])){	
+		echo '$' . round($_GET['lp'] * $_GET['f'], 2);
+		
+		}
+exit;
+
+
+
+
+
+
+?>
---
+++ rssgn.php
@@ -0,0 +1,26 @@
+<?php
+include_once('inc/functions/general.php');
+include_once 'inc/functions.php';
+error_reporting(E_ALL | E_STRICT);
+
+include_once 'inc/functions.php';
+require_once 'wconfig.php';
+
+
+	$optitlist = allqbitems();
+	if(isset($_GET['dv'])){	
+		$ds = $_GET['dv'];
+		echo '
+<select id="massign2020_'.$ds.'" name="massign2020_'.$ds.'"><option value="">Select WC Sku</option>
+'.$optitlist.'
+</select>
+<input type="button" name="massignbttn_'.$ds.'" title="'.$ds.'" value="Assign" OnClick="assign2020(this);" />';
+		}
+exit;
+
+
+
+
+
+
+?>
